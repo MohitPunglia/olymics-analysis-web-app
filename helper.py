c16d8ae4113a6cfc462e7ba28619e826a4b488be
Mohit Punglia <110087160+MohitPunglia@users.noreply.github.com>
2024-09-12 19:39:46 +0100

    Update helper.py

--- helper.py
+++ helper.py
@@ -62,3 +62,12 @@ def most_successful(df, sport):
         ['index', 'Name_x', 'Sport', 'region']].drop_duplicates('index')
     x.rename(columns={'index': 'Name', 'Name_x': 'Medals'}, inplace=True)
     return x
+
+def yearwise_medal_tally(df,country):
+    temp_df = df.dropna(subset=['Medal'])
+    temp_df.drop_duplicates(subset=['Team', 'NOC', 'Games', 'Year', 'City', 'Sport', 'Event', 'Medal'], inplace=True)
+
+    new_df = temp_df[temp_df['region'] == country]
+    final_df = new_df.groupby('Year').count()['Medal'].reset_index()
+
+    return final_df
