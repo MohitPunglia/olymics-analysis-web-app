888d4c40d4e2546124d50911e876b8d3fa4a0da4
Mohit Punglia <110087160+MohitPunglia@users.noreply.github.com>
2024-09-15 21:42:09 +0100

    Update helper.py

--- helper.py
+++ helper.py
@@ -91,3 +91,11 @@ def most_successful_countrywise(df, country):
     x.rename(columns={'index': 'Name', 'Name_x': 'Medals'}, inplace=True)
     return x
 
+def weight_v_height(df,sport):
+    athlete_df = df.drop_duplicates(subset=['Name', 'region'])
+    athlete_df['Medal'].fillna('No Medal', inplace=True)
+    if sport != 'Overall':
+        temp_df = athlete_df[athlete_df['Sport'] == sport]
+        return temp_df
+    else:
+        return athlete_df
